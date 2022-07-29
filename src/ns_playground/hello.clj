537b7cf95e9ae690ba2e71955907b90b4eb18108
Rajkumar Natarajan <rnatarajan@tremorvideo.com>
2022-07-29 16:15:54 -0400

    first commit

---
+++ src/ns_playground/hello.clj
@@ -0,0 +1,9 @@
+(ns ns-playground.hello
+  (:import [java.util Date]))
+
+(def addition +)
+
+(defn current-date []
+  "this function returns the current date"
+  (new Date))
+
---
+++ test/clj_lein_app_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-lein-app-project.core-test
+  (:require [clojure.test :refer :all]
+            [clj-lein-app-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
